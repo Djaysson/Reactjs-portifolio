273f7d0ec3ade9bfe9ff3f28a0cd227cda78a50f
Djaysson <djayson.dev@gmail.com>
2020-05-20 16:37:15 -0300

    Updating the project files as well as creating the app header

---
+++ src/pages/header/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import "./styles.css";
+//import logoImg from '../../assets/logo.png';
+
+const Header = () =>{
+  return(
+  <div className="container">
+   <header>
+      <nav>
+        <ul>
+          <li><a href="/header">Home</a></li>
+          <li><a href="/Sobre">Sobre</a></li>
+          <li><a href="/Trabalhos">Trabalhos</a></li>
+          <li><a href="/Contato">Contato</a></li>     
+        </ul>
+      </nav>
+      
+   </header>
+  </div>
+  );
+}
+export default Header;
