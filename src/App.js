db096042a72a4db1fbddc96d28097a962148fb8c
Djaysson <djayson.dev@gmail.com>
2020-05-20 17:34:59 -0300

    Organizing the files and creating the app footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Header from './pages/header/index';
+import Header from './components/header/index';
+import Footer from './components/footer/index';
 import "./global.css";
 function App() {
   return (
     <div>
       <Header />
-    
+      <Footer />
     </div>
   );
 }
