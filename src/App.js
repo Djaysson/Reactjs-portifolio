34e1504ede30eff462a123091555a9e9338bcd04
Djaysson <djayson.dev@gmail.com>
2020-05-27 23:56:57 -0300

    Creating the about page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Header from './components/header/index';
 import Footer from './components/footer/index';
-import Home from './pages/home/index';
+//import Home from './pages/home/index';
+import Sobre from './pages/sobre/index';
 import "./global.css";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Home />
+      <Sobre />
       <Footer />
     </div>
   );
