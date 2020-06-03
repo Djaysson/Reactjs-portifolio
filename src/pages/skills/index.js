58edf08a1d9ec4381c3316775ca766a3341930a4
Djaysson <djayson.dev@gmail.com>
2020-06-03 00:08:02 -0300

    Creating skill progress bar

---
+++ src/pages/skills/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {CircleProgress} from 'react-gradient-progress';
+import './styles.css';
+
+
+const Habilidades= () => {
+  return(
+  
+    <div className="progresso">
+      <h2>Habilidades</h2>
+      <div className="progresHabilidades">
+          <div className="percetual">
+            <strong>HTML</strong>
+            <CircleProgress 
+            percentage={80} 
+            strokeWidth={8} 
+            width={100} 
+            secondaryColor="#f0f0f0" 
+            primaryColor={["#59C173","#a17fe0","#5D26C1"]}  
+            /> 
+          </div>
+          <div className="percetual">
+            <strong>CSS</strong>
+            <CircleProgress
+             percentage={70}
+              strokeWidth={8}
+               width={100} 
+               secondaryColor="#f0f0f0" 
+               /> 
+            </div>
+          <div className="percetual">
+            <strong>JAVASCRIPT</strong>
+            <CircleProgress percentage={70} strokeWidth={8} width={100} secondaryColor="#f0f0f0" /> 
+          </div>
+          <div className="percetual">
+            <strong>REACTJS</strong>
+            <CircleProgress percentage={60} strokeWidth={8} width={100} secondaryColor="#f0f0f0" /> 
+          </div>
+          <div className="percetual">
+            <strong>REACT NATIVE</strong>
+            <CircleProgress percentage={60} strokeWidth={8} width={100} secondaryColor="#f0f0f0" /> 
+          </div>
+       </div>
+    </div>
+   
+  );
+}
+export default Habilidades;
--- src/pages/sobre/index.js
+++ src/pages/sobre/index.js
@@ -1,31 +1,34 @@
 import React from 'react';
-
+import Habilidades from '../skills/index';
 import './styles.css';
 import Img from '../../assets/img.jpg';
 
 const Sobre = () => {
   return(
-    <div className="sobre">
-      <img src={Img} alt="perfil"/>
-      <div className="conteudo">
-        <h2><strong>Sobre</strong>Mim</h2>
-        
-        <p>Sou desenvolvedor Frontend de aplicações web e mobile,
-          tenho 27 anos, graduando em Análise e Desenvolvimento de
-          sistemas, apaixonado por novas tecnologias, especialmente 
-          JavaScript, React, React Native. Minha história na tecnologia
-          começa no início de 2018 quando eu decidir fazer realmente uma 
-          faculdade na área de ti. No começo não sabia muito sobre código, 
-          mais com o passar do tempo eu comecei a ver alguns linguagem de 
-          programação como C, java, javacript etc... Isso mim despertou a 
-          curiosidade de saber mais e mais sobre diferentes tecnologia e logo
-          após isso entrei de cabeça nesse mundo de infinitas possibilidades,
-          na ocasião cabei gostado da stack javascript ,pois com a mesma é possível
-          fazer diferentes projeto que vai de frontend, backend, mobile, com isso eu 
-          acabei ficando maravilhado com essa stack até chega nesse exato momento onde 
-          sou desenvolvedor.</p>
-        </div>
-    </div>
+    <>
+      <div className="sobre">
+        <img src={Img} alt="perfil"/>
+        <div className="conteudo">
+          <h2><strong>Sobre</strong>Mim</h2>
+          
+          <p>Sou desenvolvedor Frontend de aplicações web e mobile,
+            tenho 27 anos, graduando em Análise e Desenvolvimento de
+            sistemas, apaixonado por novas tecnologias, especialmente 
+            JavaScript, React, React Native. Minha história na tecnologia
+            começa no início de 2018 quando eu decidir fazer realmente uma 
+            faculdade na área de ti. No começo não sabia muito sobre código, 
+            mais com o passar do tempo eu comecei a ver alguns linguagem de 
+            programação como C, java, javacript etc... Isso mim despertou a 
+            curiosidade de saber mais e mais sobre diferentes tecnologia e logo
+            após isso entrei de cabeça nesse mundo de infinitas possibilidades,
+            na ocasião acabei gostado da stack javascript, pois com a mesma é possível
+            fazer diferentes projeto que vai de frontend, backend, mobile, com isso eu 
+            acabei ficando maravilhado com essa stack até chega nesse exato momento onde 
+            sou desenvolvedor.</p>
+          </div>        
+      </div>
+      <Habilidades />
+    </>
   );
 }
 export default Sobre;
