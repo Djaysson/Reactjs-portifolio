6758f480cd0093138aa555a2c4c0ee421624b54b
Djaysson <djayson.dev@gmail.com>
2020-06-05 21:44:17 -0300

    Updating files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Header from './components/header/index';
 import Footer from './components/footer/index';
-//import Home from './pages/home/index';
-import Sobre from './pages/sobre/index';
+import Home from './pages/home/index';
+//import Sobre from './pages/sobre/index';
 import "./global.css";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Sobre />
+      <Home />
       <Footer />
     </div>
   );
