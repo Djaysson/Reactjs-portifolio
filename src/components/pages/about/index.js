2d32f9f6ab6df39c6f1ab605c544da504b9e2df3
Djaysson <djayson.dev@gmail.com>
2020-07-24 19:42:19 -0300

    Inserting animations on the home page

--- src/components/pages/about/index.js
+++ src/components/pages/about/index.js
@@ -4,28 +4,27 @@ import './styles.css';
 import Img from '../../../assets/img.jpg';
 
 const Sobre = () => {
-  return(
+  return (
     <>
       <div className="sobre">
-        <img src={Img} alt="perfil"/>
+        <img src={Img} alt="perfil" />
         <div className="conteudo">
           <h2><strong>Sobre</strong>Mim</h2>
-          
-          <p>Sou desenvolvedor Frontend de aplicações web e mobile,
-            tenho 27 anos, graduando em Análise e Desenvolvimento de
-            sistemas, apaixonado por novas tecnologias, especialmente 
-            JavaScript, React, React Native. Minha história na tecnologia
-            começa no início de 2018 quando eu decidir fazer realmente uma 
-            faculdade na área de ti. No começo não sabia muito sobre código, 
-            mais com o passar do tempo eu comecei a ver alguns linguagem de 
-            programação como C, java, javacript etc... Isso mim despertou a 
-            curiosidade de saber mais e mais sobre diferentes tecnologia e logo
-            após isso entrei de cabeça nesse mundo de infinitas possibilidades,
-            na ocasião acabei gostado da stack javascript, pois com a mesma é possível
-            fazer diferentes projeto que vai de frontend, backend, mobile, com isso eu 
-            acabei ficando maravilhado com essa stack até chega nesse exato momento onde 
-            sou desenvolvedor.</p>
-          </div>        
+
+          <p>
+            Me chamo Djayson Rodrigues sou desenvolvedor Frontend de aplicações
+            web e mobile, sou cientista da computação, tenho 27 anos, graduando
+            em Análise e Desenvolvimento de sistemas, apaixonado pelo desenvolvimento
+            de software, em especial a stack JavaScript, React, React Native e Node.js
+            e todo o ecossistema por volta dessas tecnologias.
+          </p>
+          <p>
+            Sou lifelong learner de tecnologia e apaixonado pelo que eu faço, sou
+            fascinado por novos desafios na área de desenvolvimento de software
+            principalmente nessa stack, pois com ela é possível fazer diferentes
+            projeto que vai de frontend, backend, mobile.
+          </p>
+        </div>
       </div>
       <Habilidades />
     </>
--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,33 +1,34 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 import './styles.css';
 import Img from '../../../assets/coding.jpg';
-const Home = () =>{
+const Home = () => {
   return (
     <div className="home">
-      <img src={Img} alt="backgraud"/>
-        <div className="info">
-          <h1>Ola,Eu sou</h1>
-          <h2>Djayson Rodrigues</h2>
-          <p>Developer Front End | React | React Nativo</p>
-        </div>
-        <div className="content">
-            <strong>Seja Bem vindo ao meu portifolio</strong>
-          
-           <p>Mim chamo Djayson Rodrigues e criei este
-              Portfólio com o intuito de  mostrar os
-              meus projetos que fiz como desenvolvedor. 
-              Aqui você irá conhecer 
-              um pouco da minha história profissional,
-              além de poder conferir meus trabalhos e
-              entrar em contato diretamente comigo.
-           </p>   
-           <button type="button">Saiba mais </button>      
-         
-        </div>
-       
+      <img src={Img} alt="backgraud" />
+      <div className="info">
+        <h1>Ola,Eu sou</h1>
+        <h2>Djayson Rodrigues</h2>
+        <p>Developer Front End | React | React Nativo</p>
+      </div>
+      <div className="content">
+        <strong>Seja Bem vindo ao meu portifolio</strong>
+
+        <p>Me chamo Djayson Rodrigues e criei este
+        Portfólio com o intuito de  mostrar os
+        meus projetos que fiz como desenvolvedor.
+        Aqui você irá conhecer
+        um pouco da minha história profissional,
+        além de poder conferir meus trabalhos e
+        entrar em contato diretamente comigo.
+           </p>
+        <button><Link to="/about">Saiba mais</Link></button>
+
+      </div>
+
     </div>
-    
+
   );
 }
 export default Home; 
