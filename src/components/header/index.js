45d3780b398c9db0ddad4730eecbe9e426d2058b
Djaysson <djayson.dev@gmail.com>
2020-12-11 21:43:51 -0300

    Creating the responsive menu in hamburger format and updating the navigation files

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./styles.css";
 import logoImg from '../../assets/logo.svg';
 
 const Header = () => {
-  return(
-  <div className="menu">
-  <header>
-  <img src={logoImg} alt="logotipo"/>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">Sobre</Link></li>
-          <li><Link to="/works">Trabalhos</Link></li>
-          <li><Link to="/Contact">Contato</Link></li>     
-        </ul>
-      </nav>      
-   </header>
-  </div>
+  return (
+    <div className="menu">
+      <header>
+        <img src={logoImg} alt="logotipo" />
+        <nav>
+          <input type="checkbox" id="check" />
+          <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+          </label>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">Sobre</Link></li>
+            <li><Link to="/works">Trabalhos</Link></li>
+            <li><Link to="/Contact">Contato</Link></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
   );
 }
 export default Header;
