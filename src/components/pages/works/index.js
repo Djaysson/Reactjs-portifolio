b03473e487c4aae92afc2dcb4085cc04c6a5997b
Djaysson <djayson.dev@gmail.com>
2020-12-21 18:11:55 -0300

    Updating application job page files

--- src/components/pages/works/index.js
+++ src/components/pages/works/index.js
@@ -4,6 +4,7 @@ import './styles.css';
 import Img from '../../../assets/home.png';
 import Img2 from '../../../assets/convert.png';
 import Img3 from '../../../assets/palpitebox.png';
+import Img4 from '../../../assets/pageTestesCovid.png';
 
 function Works() {
   return (
@@ -13,37 +14,48 @@ function Works() {
       <h1>Trabalhos</h1>
 
       <main className="project-list">
-        <article className="project1">
+        <article className="projects">
           <header>
             <span>Frontend</span>
             <p>Portfolio</p>
           </header>
           <img src={Img} alt="Imagem do projeto portifolio" />
           <footer>
-            <button><a href="https://portfolio-rouge-one.vercel.app/">demo</a></button>
-            <button><a href="https://github.com/Djaysson/Reactjs-portifolio">github</a></button>
+            <button><a target="_blank" href="https://portfolio.djaysson.vercel.app">demo</a></button>
+            <button><a target="_blank" href="https://github.com/Djaysson/Reactjs-portifolio">github</a></button>
           </footer>
         </article>
-        <article className="project2">
+        <article className="projects">
           <header>
             <span>Fullstack</span>
             <p>ConvertMyMoney</p>
           </header>
-          <img src={Img2} alt="Imagem do projeto portifolio" />
+          <img src={Img2} alt="Imagem do projeto ConvertMyMoney" />
           <footer>
-            <button><a href="https://convert-my-money-theta.vercel.app/">demo</a></button>
-            <button><a href="https://github.com/Djaysson/ConvertMyMoney">github</a></button>
+            <button><a target="_blank" href="https://convertmymoney.vercel.app">demo</a></button>
+            <button><a target="_blank" href="https://github.com/Djaysson/ConvertMyMoney">github</a></button>
           </footer>
         </article>
-        <article className="project3">
+        <article className="projects">
           <header>
             <span>Fullstack</span>
             <p>PalpiteBox</p>
           </header>
-          <img src={Img3} alt="Imagem do projeto portifolio" />
+          <img src={Img3} alt="Imagem do projeto PalpiteBox" />
           <footer>
-            <button><a href="https://palpite-box-orpin.vercel.app/">demo</a></button>
-            <button><a href="https://github.com/Djaysson/PalpiteBox">github</a></button>
+            <button><a target="_blank" href="https://palpite-box-orpin.vercel.app/">demo</a></button>
+            <button><a target="_blank" href="https://github.com/Djaysson/PalpiteBox">github</a></button>
+          </footer>
+        </article>
+        <article className="projects">
+          <header>
+            <span>Frontend</span>
+            <p>Testes-covid</p>
+          </header>
+          <img src={Img4} alt="Imagem do projeto Testes-covid" />
+          <footer>
+            <button><a target="_blank" href="https://testes-covid.netlify.app">demo</a></button>
+            <button><a target="_blank" href="https://github.com/Djaysson/Testes-covid">github</a></button>
           </footer>
         </article>
 
