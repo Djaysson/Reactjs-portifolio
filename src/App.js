263a92ae378c6d440b06da8064cf0f8d2151076b
Djaysson <djayson.dev@gmail.com>
2020-12-29 23:32:29 -0300

    Updating the APP file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import Header from './components/header/index';
 import "./global.css";
 import Routes from './Routes';
-import { BrowserRouter } from "react-router-dom";
-import Footer from './components/footer';
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Routes />
-        <Footer />
-      </BrowserRouter>
-    </div>
+    <Routes />
   );
 }
 
